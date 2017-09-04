f07916efec30399eb97729929935af9bf5a2eb78
Rebecca <beckylum0214@gmail.com>
2017-09-04 15:08:26 +0800

    first commit

---
+++ Assignment1Q1_BankLoan/src/assignment1q1_bankloan/Assignment1Q1_BankLoan.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment1q1_bankloan;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author rebecca
+ */
+public class Assignment1Q1_BankLoan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        char theFlag = 'y';
+        //char theInput;
+        Scanner theKB = new Scanner(System.in);
+        InterestPmt thePmt = new InterestPmt();
+        do
+        {
+            thePmt.inputScreen();
+            thePmt.displayTable();
+            theFlag = thePmt.exitProg();
+            
+        }while (theFlag == 'y');
+        
+    }
+    
+}
---
+++ Assignment1Q1_BankLoan/src/assignment1q1_bankloan/InterestPmt.java
@@ -0,0 +1,258 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment1q1_bankloan;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author rebecca
+ */
+public class InterestPmt 
+{
+    private double thePrincipal;
+    private int thePeriod;
+    private double theStartInterest;
+    private double theEndInterest;
+    Scanner theKB = new Scanner(System.in);
+
+    public double getThePrincipal() 
+    {
+        return thePrincipal;
+    }
+
+    public void setThePrincipal(double thePrincipal) 
+    {
+        this.thePrincipal = thePrincipal;
+    }
+
+    public int getThePeriod() 
+    {
+        return thePeriod;
+    }
+
+    public void setThePeriod(int thePeriod) 
+    {
+        this.thePeriod = thePeriod;
+    }
+
+    public double getTheStartInterest() 
+    {
+        return theStartInterest;
+    }
+
+    public void setTheStartInterest(double theStartInterest) 
+    {
+        this.theStartInterest = theStartInterest;
+    }
+    
+    public double getTheEndInterest() 
+    {
+        return theEndInterest;
+    }
+
+    public void setTheEndInterest(double theEndInterest) 
+    {
+        this.theEndInterest = theEndInterest;
+    }
+    
+    public void studentInfo()
+    {
+        System.out.println("Name:               Rebecca Lim");
+        System.out.println("Student Number:     33111264");
+        System.out.println("Enrolement:         Internal");
+        System.out.println("Tutor:              Mark Abernethy");
+        System.out.println("Tutorial:           Thursday, 10.30am");
+    }
+    
+    /**
+     * The method allows the user to input the principal, the interest rates,
+     * the number of years into the application.
+     */
+    public void inputScreen()
+    {
+        boolean theFlag = false; 
+        
+        do
+        {
+            System.out.println("Please enter the principal amount: ");
+            thePrincipal = theKB.nextDouble();
+            if(isInvalid(thePrincipal))
+            {
+                System.out.println("Please enter a number greater than 0:");
+            }
+
+        }while(isInvalid(thePrincipal));
+        
+        do
+        {
+            System.out.println("Please enter the rate of starting interest: ");
+            theStartInterest = theKB.nextDouble();
+            if(isInvalid(theStartInterest))
+            {
+                System.out.println("Please enter a number greater than 0:");
+            }
+        }while(isInvalid(theStartInterest));
+        
+        do
+        {
+            
+            System.out.println("Please enter the rate of ending interest: ");
+            theEndInterest = theKB.nextDouble();
+            
+            if (this.isSmaller())
+            {
+                System.out.println("Please enter an interest greater than "
+                        + "the starting interest");
+                theFlag = true;
+            }
+            else
+            {
+                theFlag = false;
+            }
+            
+            if(this.isInvalid(theEndInterest))
+            {
+                System.out.println("Please enter a number greater than 0:");
+                theFlag = true;
+            }
+            
+        }while(theFlag);
+        
+        do
+        {
+          
+            System.out.println("Please enter the number of years: ");
+            thePeriod = theKB.nextInt();
+           
+            if(isInvalid(thePeriod))
+            {
+                System.out.println("Please enter a number greater than 0:");
+            }
+        }while(isInvalid(thePeriod));
+    }
+    
+    /**
+     * 
+     * @param zPeriod is checked for whether it is less than or equal to 0.
+     * @return
+     */
+    public boolean isInvalid(int zPeriod)
+    {
+        boolean theFlag = false;
+        
+        if(zPeriod <= 0)
+        {
+            theFlag = true;
+        }
+        return theFlag;
+    }
+    
+    /**
+     *
+     * @param zAmount
+     * @return
+     */
+    public boolean isInvalid(double zAmount)
+    {
+        boolean theFlag = false;
+        
+        if(zAmount <= 0)
+        {
+            theFlag = true;
+        }
+        return theFlag;
+    }
+    
+    /**
+     *
+     * @return
+     */
+    public boolean isSmaller()
+    {
+        boolean theFlag = false;
+        
+        if(Math.abs(this.theStartInterest) > Math.abs(this.theEndInterest))
+        {
+            theFlag = true;
+        }
+        return theFlag;
+    }
+    
+    /**
+     *
+     * @param inputInt
+     * @param inputN
+     * @return
+     */
+    public double pmtCalc(double inputInt, int inputN)
+    {
+        double calcPmt;
+        
+        calcPmt = (inputInt * (thePrincipal))/(1-(Math.pow((1+inputInt), 
+                (-1*inputN))));
+        return calcPmt;
+    }
+    
+    /**
+     *
+     */
+    public void displayTable()
+    {
+        double thePmt;
+        double totPmt;
+        
+        System.out.println("Loan Amount: "+ thePrincipal);
+        System.out.println("Number of years: "+ thePeriod);
+        System.out.printf("%20s %20s %20s\n", "Interest Rate", 
+                "Monthly Payment", "Total Payment");
+        while(theStartInterest <= theEndInterest)
+        {
+            double r = (theStartInterest /12)/100;
+            int n = thePeriod*12;
+            thePmt = pmtCalc(r, n);
+            totPmt = thePmt*n;
+            System.out.printf("%20.2f%% %20.2f %20.2f\n", theStartInterest, 
+                    thePmt, totPmt);
+            theStartInterest = theStartInterest + 0.25;
+        }
+    }
+    
+    /**
+     *
+     * @return
+     */
+    public char exitProg()
+    {   
+        char theFlag = 'y';
+        char theInput;
+        do
+        {
+            System.out.println("Would you like to continue? (Y/N)");
+            theInput = theKB.next().charAt(0);
+            if((theInput == 'y') || (theInput == 'Y')|| (theInput =='n')||(theInput == 'N'))
+            {
+                if((theInput == 'n') || (theInput == 'N'))
+                {
+                    theFlag = 'n';
+                }
+                else
+                {
+                    break;
+                }
+            }
+            else
+            {
+                System.out.println("Please enter [Y/N]");
+            }
+            
+        }while(theFlag == 'y');
+        
+        return theFlag;
+    }
+    
+    
+}
